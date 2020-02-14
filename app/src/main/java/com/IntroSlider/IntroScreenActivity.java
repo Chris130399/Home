fdd7502b1a4ece2520a0721ba33ba00bd6c6bb3b
Chris <chrispius99@gmail.com>
2020-02-14 14:48:30 +0530

    This is the Home page

---
+++ app/src/main/java/com/IntroSlider/IntroScreenActivity.java
@@ -0,0 +1,16 @@
+package com.IntroSlider;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.home.R;
+
+public class IntroScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
